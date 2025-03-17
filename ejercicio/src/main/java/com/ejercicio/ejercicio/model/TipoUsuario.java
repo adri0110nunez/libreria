2d3e840ea376c4338a52abfcf39664e9e15ee214
adri0110nunez <73164098+adri0110nunez@users.noreply.github.com>
2025-03-17 19:01:22 +0100

    Delete ejercicio directory

--- ejercicio/src/main/java/com/ejercicio/ejercicio/model/TipoUsuario.java
+++
@@ -1,6 +0,0 @@
-package com.ejercicio.ejercicio.model;
-
-public enum TipoUsuario {
-    NORMAL,
-    ADMINISTRADOR
-} 
--- ejercicio/src/main/java/com/ejercicio/ejercicio/repository/LibroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ejercicio.ejercicio.repository;
-
-import com.ejercicio.ejercicio.model.Libro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LibroRepository extends JpaRepository<Libro, Long> {
-} 
